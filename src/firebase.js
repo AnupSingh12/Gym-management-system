8b7c0e9b372f5595b4b494bd43bdc8bede68aa1c
AnupSingh12 <anupsingh111.as@gmail.com>
2025-01-22 14:08:31 +0530

    created firebase configs

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDpXVVq7ik7t7wywcuUqqZrl4HGnnb1Plc",
+    authDomain: "gym-management-system-c6f73.firebaseapp.com",
+    projectId: "gym-management-system-c6f73",
+    storageBucket: "gym-management-system-c6f73.firebasestorage.app",
+    messagingSenderId: "314378890510",
+    appId: "1:314378890510:web:a711bfbcd995085fff3b5f",
+    measurementId: "G-NJRTPT5EEV"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  export const auth = getAuth(app);
+  export const db = getFirestore(app);
