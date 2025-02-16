4bf6ae9795e7a66781ba16ee75f1b50d144da4fa
AnupSingh12 <anupsingh111.as@gmail.com>
2025-02-16 13:01:15 +0530

    Modified code tso that it work perfectly

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
 import React from "react";
-import { BrowserRouter as Router , Routes , Route } from "react-router-dom";
+import { BrowserRouter as Router , Routes , Route, Navigate } from "react-router-dom";
 import AdminDashboard from "./pages/AdminDashboard";
 import UserDashboard from "./pages/UserDashboard";
 import MemberDashboard from "./pages/MemberDashboard";
-import Header from "./components/Shared/Header"
-import Footer from "./components/Shared/Footer"
+import "./styles/App.css"
 
 
 function App(){
   return(
+    
     <Router>
-    <Header/>
+      
       <Routes>
+      <Route path="/"  element={<Navigate to ="/admin"/>} />
         <Route path ="/admin" element={<AdminDashboard/>}/>
         <Route path="/member" element={<MemberDashboard/>} />
         <Route path="/user" element={<UserDashboard/>} />
       </Routes>
-    <Footer/>  
+      
     </Router>
+    
   );
 }
 
--- src/firebase.js
+++ src/firebase.js
@@ -12,6 +12,6 @@ const firebaseConfig = {
     measurementId: "G-NJRTPT5EEV"
   };
 
-  const app = initializeApp(firebaseConfig);
+ export const app = initializeApp(firebaseConfig);
   export const auth = getAuth(app);
   export const db = getFirestore(app);
